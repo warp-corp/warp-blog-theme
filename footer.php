15ffd5e6c79df98529b2a1039ab4b952d1973e7c
FoxDeveloper <development@gaudebenjamin.com>
2014-07-10 06:35:56 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+			<!-- footer -->
+			<footer id="footer" role="footer">
+				
+			</footer>
+
+			<?php wp_footer(); ?>
+			<!-- /footer -->
+
+		</section>
+		<!-- /main container -->
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head <?php language_attributes(); ?>>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		
+		<title><?php the_title(); ?></title>
+
+		<link rel="stylesheet" href=" <?php echo get_template_directory_uri(); ?>/css/knacss.css">
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<!-- main container -->
+		<section class="w60 center" id="container">
+			
+			<!-- header -->
+			<header id="header" role="header">
+				
+			</header>
+			<!-- /header -->
+	
+			<!-- main menu -->
+			<nav id="menu" role="navigation">
+				
+			</nav>
+			<!-- /main menu -->
