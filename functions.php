a2737df0baf781dec64fd14cc3aa953d769caeb5
FoxDeveloper <development@gaudebenjamin.com>
2014-07-19 16:49:30 +0200

    ajout commentaires

--- functions.php
+++ functions.php
@@ -9,3 +9,26 @@ if ( function_exists('register_sidebar') )
 		'before_title' => '<h4 class="section">',
 		'after_title' => '</h4>',
 	));
+
+function my_custom_comments($comment, $args, $depth) { 
+   $GLOBALS['comment'] = $comment; ?>
+   <div <?php comment_class(); ?> id="comment-<?php comment_ID() ?>">
+   <div class="line">
+   		<span class="left mrs wrap-avatar">
+   			<?php echo get_avatar( $comment, 64 ); ?>
+   		</span>
+   		<div class="mod">
+   			<p class="comment_author">
+               <?php comment_author(); ?> 
+   			   <span class="right muted"><i class="fa fa-clock-o"></i> <?php comment_date(); ?></span>
+            </p>	
+   			<div class="comment-body">
+   				<?php comment_text(); ?>
+   			</div>
+   		</div>
+   	</div>
+   <?php if ($comment->comment_approved == '0') : ?>
+      <em><?php _e('Your comment is awaiting moderation.') ?></em>
+   <?php endif; ?>
+   </div>
+<?php } ?>
